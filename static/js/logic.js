ae39a9f7df3e628657b608aeac7f56956842493f
Sean Rubin <seanrubi@udel.edu>
2024-06-09 14:53:45 -0400

    Current code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,34 +1,157 @@
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: stateData.map(object => object.stateID),
-    datasets: [{
-      label: "Drug Overdoses",
-      backgroundColor: "red",
-      data: stateData.map(object => object.d2015),
+function onload() {
+  new Chart("myChart", {
+    type: "bar",
+    data: {
+      labels: stateData.map(object => object.stateID),
+      datasets: [{
+        label: "Drug Overdoses",
+        backgroundColor: "red",
+        data: stateData.map(object => object.d2015),
+      },
+      {
+        label: "Crime Rate",
+          backgroundColor: "blue",
+          data: stateData.map(object => object.c2015),
+      }]
     },
-    {
-      label: "Crime Rate",
-         backgroundColor: "blue",
-         data: stateData.map(object => object.c2015),
-    }]
-  },
-  options: {
-    legend: {display: true},
-    title: {
-      display: true,
-      text: "Drug Deaths and Crime in the US"
+    options: {
+      legend: {display: true},
+      title: {
+        display: true,
+        text: "Drug Deaths and Crime in the US"
+      }
     }
-  }
-});
-var select = document.getElementById("selectNumber");
-var options = ["2015", "2016", "2017", "2018", "2019"];
-
-for(var i = 0; i < options.length; i++) {
-    var opt = options[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
+  });
 }
-console.log(el.value)
+onload();
+// Function called by DOM changes
+function getData() {
+  let Year = document.getElementById('selDataset').value
+  
+  // If/Else statement to assign the chosen country to the new dataset
+  if (Year == "2015") {
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: stateData.map(object => object.stateID),
+        datasets: [{
+          label: "Drug Overdoses",
+          backgroundColor: "red",
+          data: stateData.map(object => object.d2015),
+        },
+        {
+          label: "Crime Rate",
+            backgroundColor: "blue",
+            data: stateData.map(object => object.c2015),
+        }]
+      },
+      options: {
+        legend: {display: true},
+        title: {
+          display: true,
+          text: "Drug Deaths and Crime in the US"
+        }
+      }
+    });
+  }
+  else if (Year == "2016") {
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: stateData.map(object => object.stateID),
+        datasets: [{
+          label: "Drug Overdoses",
+          backgroundColor: "red",
+          data: stateData.map(object => object.d2016),
+        },
+        {
+          label: "Crime Rate",
+            backgroundColor: "blue",
+            data: stateData.map(object => object.c2016),
+        }]
+      },
+      options: {
+        legend: {display: true},
+        title: {
+          display: true,
+          text: "Drug Deaths and Crime in the US"
+        }
+      }
+    });
+  }
+  else if (Year == "2017") {
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: stateData.map(object => object.stateID),
+        datasets: [{
+          label: "Drug Overdoses",
+          backgroundColor: "red",
+          data: stateData.map(object => object.d2017),
+        },
+        {
+          label: "Crime Rate",
+            backgroundColor: "blue",
+            data: stateData.map(object => object.c2017),
+        }]
+      },
+      options: {
+        legend: {display: true},
+        title: {
+          display: true,
+          text: "Drug Deaths and Crime in the US"
+        }
+      }
+    });
+  }
+  else if (Year == "2018") {
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: stateData.map(object => object.stateID),
+        datasets: [{
+          label: "Drug Overdoses",
+          backgroundColor: "red",
+          data: stateData.map(object => object.d2018),
+        },
+        {
+          label: "Crime Rate",
+            backgroundColor: "blue",
+            data: stateData.map(object => object.c2018),
+        }]
+      },
+      options: {
+        legend: {display: true},
+        title: {
+          display: true,
+          text: "Drug Deaths and Crime in the US"
+        }
+      }
+    });
+  }
+  else if (Year == "2019") {
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: stateData.map(object => object.stateID),
+        datasets: [{
+          label: "Drug Overdoses",
+          backgroundColor: "red",
+          data: stateData.map(object => object.d2019),
+        },
+        {
+          label: "Crime Rate",
+            backgroundColor: "blue",
+            data: stateData.map(object => object.c2019),
+        }]
+      },
+      options: {
+        legend: {display: true},
+        title: {
+          display: true,
+          text: "Drug Deaths and Crime in the US"
+        }
+      }
+    });
+  }
+}
